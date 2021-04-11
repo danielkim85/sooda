4e7ae7c5cf2f4840d0605c8940534a09cbc6dc30
danielkim85 <danielkim@soulkast.com>
2021-04-11 01:58:08 -0400

    added install instruction

---
+++ public/javascripts/index.js
@@ -0,0 +1,14 @@
+angular.module("SoodaApp", []).controller("SoodaCtrl", function($scope) {
+  console.info('start');
+  if(window.navigator.userAgent.indexOf('iPhone') != -1){
+    console.info('i am an iPhone');
+    if(window.navigator.standalone == true){
+      console.info('i am a standalone');
+    } else {
+      console.info('i am not a standalone');
+      $scope.showInstall = true;
+    }
+  } else {
+    console.info('i am not an iphone');
+  }
+});
